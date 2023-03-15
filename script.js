b282dfac3dd0453821cc8ab2bbc4513845c7926d
Justin <jstn.anthony1@gmail.com>
2023-03-15 15:02:09 -0400

    pseudo code

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const btn = document.querySelectorAll(".radioContainer");
 
-console.log(btn);
+console.log(btn);   
+
+
+
+// JS Logic
+
+// 1.) Enter Bill amount (Ex: $50)
+// - Store amount into a variable let bill = 50 
+
+// 2.) Select 10 Percentage (Event Listener to listen for click on input use value of input to
+// divide bill based on tip amount )
+
+// Ex: 50 / 0.10: bill = 50 / 0.10
+// let tipAmount = bill / 0.10 = 5
+
+// 3.) User inputs number of people who will be sharing bill
+// We set that number into a variable let numOfPeople = 2
+
+// 4.) tipAmount = tipAmount / numOfPeople
+
+// 5.) Values for the output on the right side
+// Tip Amount = tipAmount 
+// Total = tipAmount * numOfPeople
